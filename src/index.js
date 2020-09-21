26a1a35f193b876e61985df2929003c0a8468091
Richard Fox <richard.fox@ahref.co.uk>
2020-09-20 23:33:44 -0700

    state handkler for the status ball

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const log = require('./log');
 const handleVote = require('./handlers/handleVote');
 const hasVoted = require('./handlers/hasVoted');
 
+const responses = require('./responses');
+
 const {logStartMiddleware, authorizeMiddleware, parseBodyMiddleware} = require('./middlewares');
 
 const port = config.get('port');
@@ -19,6 +21,10 @@ polka()
     .use(authorizeMiddleware)
     .post('vote/:competition/:category', handleVote)
     .get('vote/:competition/:category', hasVoted)
+    .get('state', (req, res) => {
+        responses.ok('1');
+        res.end();
+    })
     .listen(port);
 
 log.info('Server started');
