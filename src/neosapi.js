8f10e42c83ad781b18629dd82cf86ff66304c4ee
Richard Fox <richard.fox@ahref.co.uk>
2023-03-07 04:09:50 -0800

    chore(api): use new url

--- src/neosapi.js
+++ src/neosapi.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
-const userPath = "https://www.neosvr-api.com/api/users/";
+const userPath = "https://api.neos.com/api/users/";
 
 // Fetch a use record from neos' api, very simple but nice to be contained.
 export async function fetchNeosUser(userId) {
     return fetch(`${userPath}${userId}`).then((res) => res.json());
 }
 
-// https://www.neosvr-api.com/api/users/U-usutabiga/records/R-811e9068-9680-4703-9792-c92423f7e7f6
+// https://api.neos.com/api/users/U-usutabiga/records/R-811e9068-9680-4703-9792-c92423f7e7f6
 export async function fetchNeosRecord(userId, recordId) {
     return fetch(`${userPath}${userId}/records/${recordId}`).then((res) => res.json());
 }
